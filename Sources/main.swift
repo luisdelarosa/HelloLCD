fd16856d5d743c49e28bf529b64482e345ed5c81
Luis de la Rosa <luis@luisdelarosa.com>
2017-09-25 23:12:56 -0400

    Sleep for 10 seconds and then clear screen

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,7 +1,8 @@
 import SwiftyGPIO
 import HD44780LCD
+import Foundation
 
-
+// GPIO setup
 let gpios = SwiftyGPIO.GPIOs(for:.RaspberryPi3)
 var rs = gpios[.P16]!
 var e = gpios[.P22]!
@@ -10,11 +11,17 @@ var d5 = gpios[.P24]!
 var d6 = gpios[.P25]!
 var d7 = gpios[.P26]!
 
+// LCD display device setup
 let width = 16
 let height = 2
 let lcd = HD44780LCD(rs:rs,e:e,d7:d7,d6:d6,d5:d5,d4:d4,width:width, height:height)
 lcd.clearScreen()
-lcd.cursorHome()
+
+// Print out something
 // TODO fix the x/y transpositioning
-lcd.printString(x:0,y:0,what:"Hello World!",usCharSet:true)
+lcd.printString(x:0,y:0,what:"Hello world!",usCharSet:true)
 lcd.printString(x:1,y:0,what:"love,@louielouie",usCharSet:true)
+
+// Clean up after 10 seconds
+sleep(10)
+lcd.clearScreen()
